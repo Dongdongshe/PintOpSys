7af9669da74c909070241268787a1ffb7612d131
Ryan Choi <guehocho@usc.edu>
2014-09-23 17:58:30 -0700

    part1 is complete except the alarm-priority,

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -128,14 +128,14 @@ timer_sleep (int64_t ticks)
     tt = malloc(sizeof (struct timedThread)); /// allocate memory space
     tt->t = thread_current();
     tt->waketime = ticks + start;
-    printf("BLOCK THREAD %d WAKETIME %d\n", tt->t->tid, tt->waketime);
+    //printf("BLOCK THREAD %d WAKETIME %d\n", tt->t->tid, tt->waketime);
     enum intr_level old_level = intr_disable();                 /// disable the interrupt
     list_insert_ordered(&waitingList, &tt->e, waketime_less, NULL); /// put the struct into the waiting queue in order
     //list_push_back(&waitingList, &tt->e);
     thread_block();                                             /// put the current thread to sleep
     intr_set_level (old_level);
-  }else
-    printf("no need to sleep now");
+  }else{}
+    //printf("no need to sleep now");
 /****/
 
   ASSERT (intr_get_level () == INTR_ON);
@@ -245,7 +245,7 @@ wakeup(void) {
         struct list_elem *el = list_min(&waitingList, waketime_less, NULL);
         struct timedThread *tt = list_entry(el, struct timedThread, e);
         if (tt->waketime <= ticks) {
-            printf("unblock thread %d, current tick: %d, waketime: %d\n", tt->t->tid, ticks, tt->waketime);
+            //printf("unblock thread %d, current tick: %d, waketime: %d\n", tt->t->tid, ticks, tt->waketime);
             list_remove(el);
             thread_unblock(tt->t);
             //free(tt);
